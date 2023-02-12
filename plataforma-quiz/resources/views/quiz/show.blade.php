603d591cd915bbb86ba0472c67c52ccaf270fe54
Leonardo Slepack <leoslepack1@gmail.com>
2023-02-12 02:02:50 -0300

    upgrade project

--- plataforma-quiz/resources/views/quiz/show.blade.php
+++ plataforma-quiz/resources/views/quiz/show.blade.php
@@ -4,6 +4,8 @@
 <!--ADMINLTE-->
 @section('content_header')
 
+<link rel="stylesheet" href="/css/styles.css">
+
     <div class="col-md-10 offset-md-1">
         <div class="row">
             <div id="image-container" class="col-md-6">
@@ -11,8 +13,11 @@
             </div>
             <div id="info-container" class="col-md-6">
                 <h1>{{ $quiz->title }}</h1>
+                <h5>Descrição do Quiz</h5>
                 <p class="description">{{ $quiz->description }}</p>
+                <h5>Pergunta</h5>
                 <p class="question">{{ $quiz->question }}</p>
+                <h5>Respostas</h5>
                 <input type="checkbox" name="answerquiz" id="answerquiz">
                 <label for="answerquiz">{{ $quiz->answer }}</label> <br>
                 <a href="#" class="btn btn-primary">Enviar resposta</a>
